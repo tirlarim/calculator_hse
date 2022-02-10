b16b1e99452cdeed57844c382582be5fdb1cd592
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2022-02-10 20:19:48 +0300

    deleted cmake

--- c_solution/cmake-build-debug/CMakeFiles/c_solution.dir/calculator_main.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/c_solution.dir/calculator_main.c.o: \
-  /Users/ilavolodin/CLionProjects/calculator_hse/c_solution/calculator_main.c \
-  /Users/ilavolodin/CLionProjects/calculator_hse/c_solution/polish.h
