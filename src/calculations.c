e437ad5cdf8936535f0669e8783fa50346f58131
Stanislav Romanov <stasromanov801@gmail.com>
2024-06-23 04:42:26 +0200

    add pow & log

--- src/calculations.c
+++ src/calculations.c
@@ -8,8 +8,8 @@ void is_num_or_const(COMPLEX_ARR* arr, WORD* word) {
 }
 
 static int choose(const char* str) {
-  char* functions[] = {"cos", "sin", "tg", "ln", "lg", "sqrt", "abs", "exp", "u-", "real", "imag", "mag", "phase", "log"};
-  for (int i = 0; i < 14; ++i) {
+  char* functions[] = {"cos", "sin", "tg", "ln", "lg", "sqrt", "abs", "exp", "u-", "real", "imag", "mag", "phase", "log", "pow"};
+  for (int i = 0; i < 15; ++i) {
     if (strcmp(str, functions[i]) == 0) {
       return i;
     }
@@ -18,39 +18,15 @@ static int choose(const char* str) {
 }
 
 void is_f(COMPLEX_ARR* arr, WORD* word) {
-  comp (*functions[])(comp) = {ccos, csin, ctan, clog, clog10, csqrt, cabsd, cexp, u, creald, cimagd, mag, phase, empty};
+  comp (*functions[])(comp) = {ccos, csin, ctan, clog, clog10, csqrt, cabsd, cexp, u, creald, cimagd, mag, phase, empty, empty};
   const int index = choose(word->st);
   comp z = arr->arr[arr->current - 1];
   double real = creal(z);
   double image = cimag(z);
   if (image == 0.0 || image == -0.0)
-    z = real + 0.0 * I; // -0.0i give incorrect result -> 0-j
+    z = real + 0.0 * I;  // -0.0i give incorrect result -> 0-j
   arr->arr[arr->current - 1] = functions[index](z);
 }
-
-// static int choose2ArgFun(const char* str) {
-//   char* functions[] = {"pow", "log"};
-//   for (int i = 0; i < 2; ++i) {
-//     if (strcmp(str, functions[i]) == 0) {
-//       return i;
-//     }
-//   }
-//   return -1;
-// }
-//
-// void is_2arg_f(COMPLEX_ARR* arr, WORD* word) {
-//   if (arr->current - 2 < 0)
-//     return;
-//   comp (*functions[])(comp, comp) = {exponentiation, full_log};
-//   const int index = choose2ArgFun(word->st);
-//   comp z1 = arr->arr[arr->current - 2], z2 = arr->arr[arr->current - 1];
-//   if (cimag(z1) == -0.0)
-//     z1 = creal(z1) + 0.0 * I;
-//   if (cimag(z2) == -0.0)
-//     z2 = creal(z2) + 0.0 * I;
-//   arr->arr[arr->current - 2] = functions[index](z1, z2);
-//   --arr->current;
-// }
 
 static int choose_c(const char* str) {
   char* operations[] = {"+", "-", "*", "/", "^", ","};
@@ -65,6 +41,15 @@ static int choose_c(const char* str) {
 void is_operation(COMPLEX_ARR* arr, const WORD* word) {
   comp (*operations[])(comp, comp) = {plus, minus, multiplication, division, exponentiation, full_log};
   const int index = choose_c(word->st);
-  arr->arr[arr->current - 2] = operations[index](arr->arr[arr->current - 2], arr->arr[arr->current - 1]);
+  if (index == 5) {
+    if (strcmp((word + 1)->st, "log") == 0) {
+      arr->arr[arr->current - 2] = full_log(arr->arr[arr->current - 2], arr->arr[arr->current - 1]);
+    }
+    if (strcmp((word + 1)->st, "pow") == 0) {
+      arr->arr[arr->current - 2] = exponentiation(arr->arr[arr->current - 2], arr->arr[arr->current - 1]);
+    }
+  } else {
+    arr->arr[arr->current - 2] = operations[index](arr->arr[arr->current - 2], arr->arr[arr->current - 1]);
+  }
   --arr->current;
 }
--- src/calculations.h
+++ src/calculations.h
@@ -3,11 +3,8 @@
 #include "arrays.h"
 #include "calcFnAndVar/variables.h"
 
-
-void is_num_or_const(COMPLEX_ARR * arr, WORD* word);
-void is_operation(COMPLEX_ARR * arr, const WORD* word);
-void is_f(COMPLEX_ARR * arr, WORD* word);
-// void is_2arg_f(COMPLEX_ARR* arr, WORD* word);
-
+void is_num_or_const(COMPLEX_ARR* arr, WORD* word);
+void is_operation(COMPLEX_ARR* arr, const WORD* word);
+void is_f(COMPLEX_ARR* arr, WORD* word);
 
 #endif
